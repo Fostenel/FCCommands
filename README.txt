731f60f58e06ec5def778ed922a61fc784f3da7b
Joel Green <jgreen@itechsolutions.biz>
2011-12-31 16:44:56 -0700

    First commit - Initial framework

---
+++ README.txt
@@ -0,0 +1,4 @@
+FCCommands - Basic commands for a Minecraft CraftBukkit server.
+Copyright (C) 2011, 2012 Joel Green <jgreen@itechsolutions.biz>
+
+
---
+++ src/main/java/biz/itechsolutions/bukkitplugin/FCCommands/FCCommands.java
@@ -0,0 +1,64 @@
+/*
+ * FCCommands - Basic commands for a Minecraft CraftBukkit server.
+ * Copyright (C) 2011, 2012 Joel Green <jgreen@itechsolutions.biz>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package biz.itechsolutions.bukkitplugin.FCCommands;
+
+import java.util.logging.Logger;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * FCCommands
+ *
+ */
+public class FCCommands extends JavaPlugin {
+    Logger log = Logger.getLogger("Minecraft.FCCommands");
+
+    public void onDisable() {
+    	consoleMsg("Plugin Disabled");
+    }
+
+    public void onEnable() {
+        consoleMsg("Plugin Version " + this.getDescription().getVersion() + " Enabled");
+    }
+    
+    private void consoleMsg(String msg) {
+    	log.info("[" + this.getDescription().getName() + "] " + msg);
+    }
+    
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+    	Player player = null;
+    	if (sender instanceof Player) {
+    		player = (Player) sender;
+    	}
+    	
+    	if(cmd.getName().equalsIgnoreCase("FCCommands")) {
+    		if (player == null) {
+    			consoleMsg(cmd.getName() + " was run from the console!");
+    		} else {
+    			consoleMsg(cmd.getName() + " was run by " + sender.getName() + "!");
+    		}
+    		sender.sendMessage("You ran the FCCommands command!!");
+    		return true;
+    	}
+    	return false;
+    }
+}
